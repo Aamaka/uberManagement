c5e08000fe6e9e50d0c5b604d88a545dec6fa67d
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-08-10 22:23:34 +0100

    Uber

--- src/main/java/africa/semicolon/ubermanagement/data/models/User.java
+++ src/main/java/africa/semicolon/ubermanagement/data/models/User.java
@@ -1,4 +1,28 @@
 package africa.semicolon.ubermanagement.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @NotBlank @NotNull @Valid
+    @Column(name = "name")
+    private String name;
+    private String userAddress;
+    private  String email;
+    private String userContactNumber;
 }
--- src/main/java/africa/semicolon/ubermanagement/data/models/Vehicle.java
+++ src/main/java/africa/semicolon/ubermanagement/data/models/Vehicle.java
@@ -1,7 +1,5 @@
 package africa.semicolon.ubermanagement.data.models;
 
 public class Vehicle {
-    public static void main(String[] args) {
 
-    }
 }
